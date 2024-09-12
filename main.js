231a8614d2c7beb2ee5e85631d61f6a6f7155687
amhc082024 <amhc082024@gmail.com>
2024-09-12 14:03:47 +0200

    V2

--- main.js
+++ main.js
@@ -11,9 +11,31 @@ const body = response.json()
 
 
 // Konverter fra Felle Språk til JS
+const movies = await response.json()
 
 // For hver film
+for (const movie of movies) {
+    
 
 // Lage et HTML Element
+console.log(movie.title)
+
+const movieElement = document.createElement("li")
+movieElement.textContent = movie.title
+movieElement.className = "movie-card"
 
 // Hekte Elementet på nettsiden
+
+// Få tak i elementet vi ønsker å sette det inn under
+Const movieList = document.querySelector("#movie-list")
+
+movieList.append(movieElement)
+}
+
+function createMovieCard(movie) {
+const movieElement = document.createElement("li")
+movieElement.textContent = movie.title
+movieElement.className = "movie-card"
+
+return movieElement
+}
